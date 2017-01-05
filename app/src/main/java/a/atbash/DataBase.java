3530a3585bb86293ae44e03a20a5ebbdc54500b4
noambarsh <noambarsh@gmail.com>
2017-01-05 16:51:00 +0200

    no comments

--- app/src/main/java/a/atbash/DataBase.java
+++ app/src/main/java/a/atbash/DataBase.java
@@ -10,171 +10,109 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.*;
-/*
-class DataBase {
-    private SQLiteDatabase db;
-    public DataBase(SQLiteDatabase db)
-    {
-        this.db=db;
-    }
-    public int getLast()
-    {
-        db.execSQL("create table if not exists sampletable(name text, location text");
-        db.execSQL("insert into sampletable values('ran', 'hyderanad')");
-        Cursor cursor=db.rawQuery("SELECT * FROM sampletable", null);
-        cursor.moveToFirst();
-        String name = cursor.getString(0);
-        System.out.println(name);
-        int last = Integer.parseInt(cursor.getString(cursor.getColumnIndex("last")));
-        System.out.println(last);
-        return last;
-    }
-}
-*/
 public class DataBase extends SQLiteOpenHelper
 {
-
-    //The Android's default system path of your application database.
-    private static String DB_PATH = "/data/data/a.atbash/databases/";
-
+    private static String DB_PATH = "/data/data/a.atbash/databases/"; //The Android's default system path of your application database.
     private static String DB_NAME = "AtbashClient";
-
     private SQLiteDatabase myDataBase;
-
     private final Context myContext;
-
-    /**
-     * Constructor
-     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
-     * @param context
-     */
+    //constructor
     public DataBase(Context context) {
 
         super(context, DB_NAME, null, 1);
         this.myContext = context;
     }
-
-    /**
-     * Creates a empty database on the system and rewrites it with your own database.
-     * */
-    public void createDataBase() throws IOException {
-
+    //create empty database
+    public void createDataBase() throws IOException
+    {
         boolean dbExist = checkDataBase();
-
-        if(dbExist){
+        if(dbExist)
+        {
             //do nothing - database already exist
-        }else{
-
-            //By calling this method and empty database will be created into the default system path
-            //of your application so we are gonna be able to overwrite that database with our database.
+        }
+        else
+        {
             this.getReadableDatabase();
-
-            try {
-
+            try
+            {
                 copyDataBase();
-
-            } catch (IOException e) {
-
+            }
+            catch (IOException e)
+            {
                 throw new Error("Error copying database");
-
             }
         }
-
     }
-
-    /**
-     * Check if the database already exist to avoid re-copying the file each time you open the application.
-     * @return true if it exists, false if it doesn't
-     */
-    private boolean checkDataBase(){
-
+    //check if database already exist
+    private boolean checkDataBase()
+    {
         SQLiteDatabase checkDB = null;
-
-        try{
+        try
+        {
             String myPath = DB_PATH + DB_NAME;
             System.out.println(myPath);
             checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
-
-        }catch(SQLiteException e){
-
+        }
+        catch(SQLiteException e)
+        {
             //database does't exist yet.
-
         }
-
-        if(checkDB != null){
-
+        if(checkDB != null)
+        {
             checkDB.close();
-
         }
-
         return checkDB != null ? true : false;
     }
-
-    /**
-     * Copies your database from your local assets-folder to the just created empty database in the
-     * system folder, from where it can be accessed and handled.
-     * This is done by transfering bytestream.
-     * */
+    //copy database from assets to local database
     private void copyDataBase() throws IOException{
-
-        //Open your local db as the input stream
         InputStream myInput = myContext.getAssets().open(DB_NAME);
-
-        // Path to the just created empty db
         String outFileName = DB_PATH + DB_NAME;
-
-        //Open the empty db as the output stream
         OutputStream myOutput = new FileOutputStream(outFileName);
-
-        //transfer bytes from the inputfile to the outputfile
         byte[] buffer = new byte[1024];
         int length;
-        while ((length = myInput.read(buffer))>0){
+        while ((length = myInput.read(buffer))>0)
+        {
             myOutput.write(buffer, 0, length);
         }
-
         //Close the streams
         myOutput.flush();
         myOutput.close();
         myInput.close();
-
     }
 
-    public void openDataBase() throws SQLException{
-
-        //Open the database
+    //open database
+    public void openDataBase() throws SQLException
+    {
         String myPath = DB_PATH + DB_NAME;
         System.out.println(myPath);
         myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
 
     }
 
+    //close database
     @Override
-    public synchronized void close() {
-
+    public synchronized void close()
+    {
         if(myDataBase != null)
             myDataBase.close();
-
         super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
 
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
 
     }
     public Cursor x()
     {
         return myDataBase.rawQuery("SELECT * FROM Level", null);
     }
-    // Add your public helper methods to access and get content from the database.
-    // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
-    // to you to create adapters for your views.
     public Cursor queryForStage(int num)
     {
         return myDataBase.rawQuery("SELECT * FROM Level where numberOfQuestion=="+num, null);
@@ -190,8 +128,6 @@ public class DataBase extends SQLiteOpenHelper
         String Question=cur.getString(cur.getColumnIndex("Question"));
         String Hint=cur.getString(cur.getColumnIndex("Hint"));
         Stage s=new Stage(number,Question,Hint,Answer);
-        //gitrcnoam2
         return s;
     }
-
 }
