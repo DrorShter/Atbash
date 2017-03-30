ba5585b10ff7e99601e5f8e037b567caaf8c016a
Dror <dror.shter@gmail.com>
2017-03-30 18:14:30 +0300

    noam nonsence1000

--- app/src/main/java/a/atbash/StageDAL.java
+++ app/src/main/java/a/atbash/StageDAL.java
@@ -1,105 +1,88 @@
 package a.atbash;
 
-import android.app.Application;
+import android.content.Context;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
-public class StageDAL extends Application
+public class StageDAL
 {
-    private Connection connection = null;
-    private ResultSet resultSet;
-    private PreparedStatement preparedStatement = null;
-
-    private Connection getConnection() {
-        Connection con=null;
-        //Context context = getApplicationContext();
-        /*
-        String name = "AtbashClient2.db";
-        String DB_PATH = "/data/user/0/a.atbash/files/";
-        System.out.println(DB_PATH+name);
-        try {
-            DriverManager.registerDriver((Driver) Class.forName("org.sqlite.JDBC").newInstance());
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to register SQLDroidDriver");
+    private ObjectMapper mapper=null;
+    private File path;
+    public StageDAL(Context context)
+    {
+        path=new File(context.getFilesDir(), "stages");
+        mapper=new ObjectMapper();
+    }
+    
+    public Stage getStage(int num) throws IOException {
+        ObjectMapper mapper=new ObjectMapper();
+        File file = new File(path, num + ".json");
+        return mapper.readValue(file, Stage.class);
+    }
+    public int getCurrentLevel() {
+        File file = new File(path, "currentLevel.txt");
+        if(!file.exists())
+        {
+            FileOutputStream stream = null;
+            try {
+                stream = new FileOutputStream(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                stream.write("1".getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return 1;
         }
+        else
+        {
+            Scanner scanner= null;
+            try {
+                scanner = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            String s=scanner.nextLine();
+            return Integer.parseInt(s);
+        }
+    }
+    public void updateLastLevel(int cur)
+    {
+        File file = new File(path, "currentLevel.txt");
         try {
-            con = DriverManager.getConnection("jdbc:sqlite:" +DB_PATH+ "/AtbashClient2.db");
-        } catch (SQLException e) {
+            int tmp= getCurrentLevel();
+            if(tmp<cur)
+            {
+                FileOutputStream stream = new FileOutputStream(file, false);
+                stream.write(cur);
+                stream.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        */
-        return con;
-    }
-    public StageDAL()
-    {
-        connection=getConnection();
     }
 
-    public int getCurrentLevel()
-    {
-        /*
-        String query="SELECT * FROM lastLevel";
-        preparedStatement=connection.prepareStatement(query);
-        resultSet=preparedStatement.executeQuery();
-        int last=Integer.parseInt(resultSet.getString("last"));
-        return last;
-        */
-        return 3; //should be from database
-    }
-    public Stage getStage(int num)
-    {
-        /*
-        String question, answer, clue, query="SELECT * FROM Level WHERE NumberOfQuestion=?";
-        preparedStatement=connection.prepareStatement(query);
-        preparedStatement.setInt(1, num);
-        resultSet= preparedStatement.executeQuery();
-        question=resultSet.getString("Question");
-        answer=resultSet.getString("Answer");
-        clue=resultSet.getString("Clue");
-        return new Stage(num, question, clue, answer);
-        */
-        Stage ret = null;
-        switch (num)
-        {
-            case 1: ret = new Stage(1, "א", "ב", "ג");
-                break;
-            case 2: ret = new Stage(2, "מ", "נ", "ס");
-                break;
-            case 3: ret = new Stage(3, "ק", "ר", "ש");
-                break;
+    public void updateStagesFromServer(List<Stage> stages) throws IOException {
+        for (Stage stage : stages) {
+            File file = new File(path, stage.getNumber() + ".json");
+            mapper.writeValue(file, stage);
         }
-        return ret;
-    }
-    public void updateLastLevel(int curLevel)
-    {
-        /*
-        String query="UPDATE lastLevel set last=?";
-        preparedStatement=connection.prepareStatement(query);
-        preparedStatement.setInt(1, curLevel);
-        preparedStatement.executeQuery();
-        */
-    }
-    public void updateStagesFromServer(List <Stage> list)
-    {
-        System.out.println("from server");
-        System.out.println(list.get(0).getNumber());
-        System.out.println(list.get(0).getQuestion());
-        System.out.println(list.get(0).getClue());
-        System.out.println(list.get(0).getAnswer());
-        System.out.println(list.get(1).getNumber());
-        System.out.println(list.get(1).getQuestion());
-        System.out.println(list.get(1).getClue());
-        System.out.println(list.get(1).getAnswer());
-        System.out.println(list.get(2).getNumber());
-        System.out.println(list.get(2).getClue());
-        System.out.println(list.get(2).getQuestion());
-        System.out.println(list.get(2).getAnswer());
-        //work for dror
     }
 }
